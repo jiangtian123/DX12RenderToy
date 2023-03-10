f15e18f0c53f2ce82f7c3ed6a72d3cd7e0890ed1
jiangtian123 <1242713948@qq.com>
2023-03-10 19:19:26 +0800

    新增一个文件

--- Dx12RenderToy/Dx12RenderToy/Main.cpp
+++ Dx12RenderToy/Dx12RenderToy/Main.cpp
@@ -15,16 +15,18 @@ using namespace Microsoft::WRL;
 #pragma comment(lib, "dxguid.lib")
 #pragma comment(lib, "dxgi.lib")
 //
-#include"Object.h"
 #if defined(_DEBUG)
 #include <dxgidebug.h>
 #endif
+
+#include "..\WindowsCommons\d3dx12.h"
 #define GRS_WND_CLASS_NAME _T("Game Window Class")
 #define GRS_WND_TITLE   _T("DirectX12 Trigger Sample")
 
 #define GRS_THROW_IF_FAILED(hr) if (FAILED(hr)){ throw CGRSCOMException(hr); }
 
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -55,8 +57,7 @@ int WINAPI WinMain(
 
 	ComPtr<IDXGIFactory5>				pIDXGIFactory5;
 	ComPtr<IDXGIAdapter1>				pIAdapter;
-	ComPtr<ID3D12Device4>				pID3D12Device;
-	ComPtr<ID3D12CommandQueue>			pICMDQueue;
+	ComPtr<ID3D12Device4>				pID3DDevice;
 	ComPtr<IDXGISwapChain1>				pISwapChain1;
 	ComPtr<IDXGISwapChain3>				pISwapChain3;
 	ComPtr<ID3D12DescriptorHeap>		pIRTVHeap;
@@ -64,8 +65,10 @@ int WINAPI WinMain(
 	ComPtr<ID3D12RootSignature>			pIRootSignature;
 	ComPtr<ID3D12PipelineState>			pIPipelineState;
 
+	ComPtr<ID3D12CommandQueue>			pICMDQueue;
 	ComPtr<ID3D12CommandAllocator>		pICMDAlloc;
 	ComPtr<ID3D12GraphicsCommandList>	pICMDList;
+
 	ComPtr<ID3D12Resource>				pIVertexBuffer;
 	ComPtr<ID3D12Fence>					pIFence;
 
@@ -102,6 +105,64 @@ int WINAPI WinMain(
 			break;
 		}
 	}
-	D3D12CreateDevice(pIAdapter.Get(),D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pID3D12Device));
+#if defined(DEBUG) || defined(_DEBUG)
+	{
+		ComPtr<ID3D12Debug> debugController;
+		D3D12GetDebugInterface(IID_PPV_ARGS(&debugController));
+		debugController->EnableDebugLayer();
+	}
+#endif
+	//创建硬件设备
+	HRESULT hadrdwareResult = D3D12CreateDevice(pIAdapter.Get(),D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pID3DDevice));
+	if (FAILED(hadrdwareResult))
+	{
+		ComPtr<IDXGIAdapter> pWarpAdapter;
+		pIDXGIFactory5->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter));
+		D3D12CreateDevice(pWarpAdapter.Get(), D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pID3DDevice));
+	}
+	//创建Fence
+	pID3DDevice->CreateFence(0,D3D12_FENCE_FLAG_NONE,IID_PPV_ARGS(&pIFence));
 
+
+	//检测对MSAA质量级别的支持
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLecels;
+	//msQualityLecels.Format = 
+	//创建命令队列
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	//------------ D3D12_COMMAND_LIST_TYPE_DIRECT 意味着创建的Queue可以执行GPU的所有命令 -------------
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	pID3DDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&pICMDQueue));
+	//创建交换链
+	//=======================================================
+	// D3D12中创建一个交换链时，需要指定一个命令队列
+	// 最终呈现画面前，交换链需要确定绘制操作是否完全完成了，需要队列Flush一下。
+	//=======================================================
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.BufferCount = nFrameBackBuffferCount;
+	swapChainDesc.Width = iWidth;
+	swapChainDesc.Height = iHeight;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	//----- 多重采样级别 ------------------------------
+	swapChainDesc.SampleDesc.Count = 1;
+	pIDXGIFactory5->CreateSwapChainForHwnd(pICMDQueue.Get(), hWnd,&swapChainDesc,nullptr,nullptr,&pISwapChain1);
+	pISwapChain1.As(&pISwapChain3);
+	nFrameIndex = pISwapChain3->GetCurrentBackBufferIndex();
+	//创建描述符堆和描述符
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+	rtvHeapDesc.NumDescriptors = nFrameBackBuffferCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	pID3DDevice->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&pIRTVHeap));
+	//获取每个RTV的实际大小
+	nRTVDescriptorSize = pID3DDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	//创建描述符
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(pIRTVHeap->GetCPUDescriptorHandleForHeapStart());
+	for (size_t i = 0; i < nFrameBackBuffferCount; i++)
+	{
+		pISwapChain3->GetBuffer(i,IID_PPV_ARGS(&pIARenderTargets[i]));
+		pID3DDevice->CreateRenderTargetView(pIARenderTargets[i].Get(),nullptr,rtvHeapHandle);
+		rtvHeapHandle.Offset(1, nRTVDescriptorSize);
+	}
 }
